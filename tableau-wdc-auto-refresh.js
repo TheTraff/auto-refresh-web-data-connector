df4da81e1bb49643091397c0d99cfce2558aa0a7
Ben Traffanstedt <btraffanstedt@proctoru.com>
2020-04-29 14:37:58 -0500

    Found a more recent version of the wdc on the original author's github
    * supposedly fixes some issues with 10.5, which is the version
      tableau changed to the newer method of loading a wdc

---
+++ tableau-wdc-auto-refresh.js
@@ -0,0 +1,34 @@
+var countdown =  $("#countdown").countdown360({
+    onComplete  : function () {  
+        getCurrentViz().refreshDataAsync();
+        
+        var target = window.parent.document.getElementById("loadingSpinner");
+        if (!$(target).is(':visible')) {
+            countdown.start();
+        } else {
+            var observer = new MutationObserver(function(mutations) {
+              mutations.forEach(function(mutationRecord) {
+                if (!$(target).is(':visible')) {
+                  observer.disconnect();
+                  countdown.start();
+                }
+              });    
+            });
+            var observerConfig = {
+              attributes: true, 
+              attributeFilter: ['style'],
+              childList: false, 
+              subtree: false 
+            };
+            observer.observe(target, observerConfig);
+        }
+    }
+});
+$('#countdown').click(function(e){
+    countdown.getStatus() ? countdown.pause() : countdown.cont();
+});
+
+$('#countdown').on("mouseover", function(e) {countdown.showControls(true);});
+$('#countdown').on("mouseout", function(e) {countdown.showControls(false);});
+
+$(document).ready(function() { appApi.initAutoRefresh();} );
