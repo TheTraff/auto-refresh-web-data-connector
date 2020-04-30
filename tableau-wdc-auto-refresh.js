284f1e39bd41458320de9396f1dfb75508d92d77
Ben Traffanstedt <btraffanstedt@proctoru.com>
2020-04-30 16:03:53 -0500

    boop

--- tableau-wdc-auto-refresh.js
+++ tableau-wdc-auto-refresh.js
@@ -31,19 +31,42 @@ $('#countdown').click(function(e){
 $('#countdown').on("mouseover", function(e) {countdown.showControls(true);});
 $('#countdown').on("mouseout", function(e) {countdown.showControls(false);});
 
+$(document).ready(function() {
+
+  // Hook up an event handler for the load button click.
+  // Wait to initialize until the button is clicked.
+  $("#initializeButton").click(function() {
+
+    // Disable the button after it's been clicked
+    $("#initializeButton").prop('disabled', true);
+
+    tableau.extensions.initializeAsync().then(function() {
+
+      // Initialization succeeded! Get the dashboard
+      var dashboard = tableau.extensions.dashboardContent.dashboard;
+
+      // Display the name of dashboard in the UI
+      $("#resultBox").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
+    }, function(err) {
+
+      // something went wrong in initialization
+      $("#resultBox").html("Error while Initializing: " + err.toString());
+    });
+  });
+});
+
+
+/*
 (function () {
 	$(document).ready(function () {
 		tableau.extensions.initializeAsync().then(function () {
-		/* body of function  */
-    /* controls what the extension does */
-    /* extension calls other functions here */ 
 
     } function (err) {
       // Something went wrong in initialization.
       console.log('Error while Initializing: ' + err.toString());
     });
 });
-/*  extension can define other functions here as needed */
 })();
+*/
 
 
