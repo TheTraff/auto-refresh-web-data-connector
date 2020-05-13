348816c3540fa448479e7684e8fd50b9ad2f632f
Ben Traffanstedt <btraffanstedt@proctoru.com>
2020-05-13 11:40:54 -0500

    removed tutorial stuff

--- tableau-wdc-auto-refresh.js
+++ tableau-wdc-auto-refresh.js
@@ -79,29 +79,18 @@ $('#countdown').on("mouseover", function(e) {countdown.showControls(true);});
 $('#countdown').on("mouseout", function(e) {countdown.showControls(false);});
 
 $(document).ready(function() {
+  tableau.extensions.initializeAsync().then(function() {
 
-  // Hook up an event handler for the load button click.
-  // Wait to initialize until the button is clicked.
-  $("#initializeButton").click(function() {
-
-    // Disable the button after it's been clicked
-    $("#initializeButton").prop('disabled', true);
-
-    tableau.extensions.initializeAsync().then(function() {
-
-      // Initialization succeeded! Get the dashboard
-      var dashboard = tableau.extensions.dashboardContent.dashboard;
-      initAutoRefresh();
+    // Initialization succeeded! Get the dashboard
+    var dashboard = tableau.extensions.dashboardContent.dashboard;
+    initAutoRefresh();
 
 
+    console.log("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
+  }, function(err) {
 
-      // Display the name of dashboard in the UI
-      $("#resultBox").html("I'm running in a dashboard named <strong>" + dashboard.name + "</strong>");
-    }, function(err) {
-
-      // something went wrong in initialization
-      $("#resultBox").html("Error while Initializing: " + err.toString());
-    });
+    // something went wrong in initialization
+    console.log("Error while Initializing: " + err.toString());
   });
 });
 
